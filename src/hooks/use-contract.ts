7e1a9eaffdf2704f27be80f7fa34278875b5bd6c
emmanuel taiwo <laffbaze@gmail.com>
2024-09-08 15:26:01 +0100

    Network fix

--- src/hooks/use-contract.ts
+++ src/hooks/use-contract.ts
@@ -1,5 +1,5 @@
-import { useReadContract, useWriteContract } from "wagmi"
-import  projectAbi from "../abis/projectAbi.json" 
+import { useReadContract, useWriteContract } from "wagmi";
+import projectAbi from "../abis/projectAbi.json";
 import { CONTRACT_ADDRESS } from "@/constants";
 import { useEffect, useState } from "react";
 import { readContract } from "wagmi/actions";
@@ -8,165 +8,153 @@ import { FormSchema } from "@/pages/create";
 import { z } from "zod";
 // import { toast } from "@/components/ui/use-toast";
 
-
-export interface RecordI{
-  recordId:string;
+export interface RecordI {
+  recordId: string;
   patientName: string;
-  diagnosis:string;
-  patientDOB:string;
-  patientGender:string;
-  createdBy?:string;
-  createdAt?:number;
-  viewsCount?:number;
+  diagnosis: string;
+  patientDOB: string;
+  patientGender: string;
+  createdBy?: string;
+  createdAt?: number;
+  viewsCount?: number;
 }
 
 interface ResI {
   [key: number]: string;
 }
 
-export const useRecords = ()=>{
-  const [records,setRecords]  = useState<RecordI[]>([]) 
+export const useRecords = () => {
+  const [records, setRecords] = useState<RecordI[]>([]);
   const [loading, setLoading] = useState(false);
 
-  const getRecords =async ()=>{
-    setLoading(true)
-     try {
-      setRecords([])
-      const ids:string[] = await readContract(config,{
+  const getRecords = async () => {
+    setLoading(true);
+    try {
+      setRecords([]);
+      const ids: string[] = (await readContract(config, {
         abi: projectAbi,
-        functionName: 'getAllRecordIds',
-        address: CONTRACT_ADDRESS
-      }) as string[];
-      
-   
-  
-      ids.forEach(async (id:string) => {
-      
-       const records = await  getRecordByid(id);
-        setRecords((prev:RecordI[])=>{
-          return [...prev,
+        functionName: "getAllRecordIds",
+        address: CONTRACT_ADDRESS,
+      })) as string[];
+
+      ids.forEach(async (id: string) => {
+        const records = await getRecordByid(id);
+        setRecords((prev: RecordI[]) => {
+          return [
+            ...prev,
             {
-              recordId:records[0],
-              patientName:records[1],
-              diagnosis:records[2],
-              patientDOB:records[3],
-              patientGender:records[4],
-              createdBy:records[5],
-              viewsCount:parseInt(records[8]),
-              createdAt:parseInt(records[6].toString()) * 1000,
-            }
-          ]
-        })
+              recordId: records[0],
+              patientName: records[1],
+              diagnosis: records[2],
+              patientDOB: records[3],
+              patientGender: records[4],
+              createdBy: records[5],
+              viewsCount: parseInt(records[8]),
+              createdAt: parseInt(records[6].toString()) * 1000,
+            },
+          ];
+        });
       });
-       setLoading(false)
-     } catch (error) {
-       setLoading(false)
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
       console.log(error);
-     }
-   }
+    }
+  };
 
-
-   const getRecordByid = async (id:string)=>{
-    
-    const record = await readContract(config,{
+  const getRecordByid = async (id: string) => {
+    const record = (await readContract(config, {
       abi: projectAbi,
-      functionName: 'records',
+      functionName: "records",
       address: CONTRACT_ADDRESS,
-      args:[id]
-    }) as ResI; 
-    
-    
-    return record
-   }
+      args: [id],
+    })) as ResI;
 
-  useEffect(()=>{
-    getRecords()
-  },[])
+    return record;
+  };
 
-
-
-  
+  useEffect(() => {
+    getRecords();
+  }, []);
 
   return {
     records,
     loading,
-    getRecordByid
-  }
+    getRecordByid,
+  };
+};
 
- 
-}
-
-export const useRecordsCount = ()=>{
-
-  const { data,isLoading,
-    isError} = useReadContract({
+export const useRecordsCount = () => {
+  const { data, isLoading, isError } = useReadContract({
     abi: projectAbi,
-    functionName: 'getAllRecordIds',
-    address: CONTRACT_ADDRESS
-  })
-    
-    console.log(data)
-    return {
-      data,
-      isLoading,
-      isError,
-      
-    }
-}
+    functionName: "getAllRecordIds",
+    address: CONTRACT_ADDRESS,
+  });
 
-export const useCreateRecord = ()=>{
-  const { data: hash, isPending,isSuccess,isError,writeContract } = useWriteContract()
+  console.log(data);
+  return {
+    data,
+    isLoading,
+    isError,
+  };
+};
 
+export const useCreateRecord = () => {
+  const {
+    data: hash,
+    isPending,
+    isSuccess,
+    isError,
+    writeContract,
+    status,
+  } = useWriteContract();
 
-  const create = async  (data:z.infer<typeof FormSchema>)=>{
-     try {
-    await  writeContract({
+  const create = async (data: z.infer<typeof FormSchema>) => {
+    try {
+      await writeContract({
         abi: projectAbi,
-        functionName: 'createRecord',
+        functionName: "createRecord",
         address: CONTRACT_ADDRESS,
-        args: [data.patient_name,data.diagnosis,data.dob,data.gender],
-      })
-      
-     } catch (error) {
-      console.log(error)
-     }
-  }
-     const update = async  (data:z.infer<typeof FormSchema>,id?:string)=>{
-      try {
-       await  writeContract({
-         abi: projectAbi,
-         functionName: 'updateRecord',
-         address: CONTRACT_ADDRESS,
-         args: [id,data.diagnosis,data.dob,data.gender],
-       });
-    
-      } catch (error) {
-       console.log(error)
-      }
+        args: [data.patient_name, data.diagnosis, data.dob, data.gender],
+      });
+    } catch (error) {
+      console.log(error);
     }
-
-
-    const signRecord = async  (id:string)=>{
-      try {
-       await  writeContract({
-         abi: projectAbi,
-         functionName: 'getRecord',
-         address: CONTRACT_ADDRESS,
-         args: [id],
-       });
-    
-      } catch (error) {
-       console.log(error)
-      }
+  };
+  const update = async (data: z.infer<typeof FormSchema>, id?: string) => {
+    try {
+      await writeContract({
+        abi: projectAbi,
+        functionName: "updateRecord",
+        address: CONTRACT_ADDRESS,
+        args: [id, data.diagnosis, data.dob, data.gender],
+      });
+    } catch (error) {
+      console.log(error);
     }
- 
+  };
 
-
-    return {
-      create,update,
-      hash,
-      isPending,
-      isSuccess,isError ,
-      signRecord
+  const signRecord = async (id: string) => {
+    try {
+      await writeContract({
+        abi: projectAbi,
+        functionName: "getRecord",
+        address: CONTRACT_ADDRESS,
+        args: [id],
+      });
+    } catch (error) {
+      console.log(error);
     }
-}
+  };
+
+  return {
+    create,
+    update,
+    hash,
+    isPending,
+    isSuccess,
+    isError,
+    signRecord,
+    status,
+  };
+};
--- src/pages/search-page.tsx
+++ src/pages/search-page.tsx
@@ -1,129 +1,163 @@
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
   FormField,
   FormItem,
   FormMessage,
-} from "@/components/ui/form"
+} from "@/components/ui/form";
 
-import moment from 'moment';
+import moment from "moment";
 
-import { Input } from "@/components/ui/input"
-import { toast } from "@/components/ui/use-toast"
-import { RecordI, useCreateRecord, useRecords } from "@/hooks/use-contract"
-import { useNavigate } from "react-router"
+import { Input } from "@/components/ui/input";
+import { toast } from "@/components/ui/use-toast";
+import { RecordI, useCreateRecord, useRecords } from "@/hooks/use-contract";
+import { useNavigate } from "react-router";
 
-import { useState } from "react"
+import { useState } from "react";
 // import { useNavigate } from "react-router"
 
 export const FormSchema = z.object({
   query: z.string(),
-})
+});
 
-export const  SearchPage = () =>    {
-    const {isPending,isError,signRecord } = useCreateRecord();
-    const {getRecordByid}= useRecords();
-   const navigate = useNavigate();
-   const [record,setRecord] = useState<RecordI>()
-   const [error,setError] = useState('')
+export const SearchPage = () => {
+  const { isPending, isError, signRecord, status } = useCreateRecord();
+  const { getRecordByid } = useRecords();
+  const navigate = useNavigate();
+  const [record, setRecord] = useState<RecordI>();
+  const [error, setError] = useState("");
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
     defaultValues: {
       query: "",
     },
-  })
+  });
+  const [data, setData] = useState({ query: "" });
 
   async function onSubmit(data: z.infer<typeof FormSchema>) {
-   try{
-    setError('')
-    setRecord(undefined)
-    navigate({
-        pathname: '/records/search',
+    try {
+      setError("");
+      setRecord(undefined);
+      navigate({
+        pathname: "/records/search",
         search: `?s=${data.query}`,
       });
-      await  signRecord(data.query); 
-       const record = await getRecordByid(data.query);
+      await signRecord(data.query);
 
-        if(record[0] == "0x00000000000000"){
-            setError('Record not found')
-        }else{
-            setRecord({
-                recordId:record[0],
-                patientName:record[1],
-                diagnosis:record[2],
-                patientDOB:record[3],
-                patientGender:record[4],
-                createdAt: parseInt(record[6].toString()) * 1000,
-            })
-        }
-    }catch(e){
-    console.log({e})
-    setError('Invalid Record id')
+      setData(data);
+    } catch (e) {
+      console.log({ e });
+      setError("Invalid Record id");
     }
-
   }
 
-
-  if(isError){
+  if (isError) {
     toast({
-      variant:'destructive',
-      title: "Uh oh! Something went wrong", 
-      description:'There was a problem with your request'})
+      variant: "destructive",
+      title: "Uh oh! Something went wrong",
+      description: "There was a problem with your request",
+    });
   }
 
- 
+  const getRecord = async () => {
+    const record = await getRecordByid(data.query);
+
+    if (record[0] == "0x00000000000000") {
+      setError("Record not found");
+    } else {
+      setRecord({
+        recordId: record[0],
+        patientName: record[1],
+        diagnosis: record[2],
+        patientDOB: record[3],
+        patientGender: record[4],
+        createdAt: parseInt(record[6].toString()) * 1000,
+      });
+    }
+  };
+
+  if (status == "success") {
+    getRecord();
+  }
 
   return (
-      <section className="sm:max-w-[512px] w-full bg-white rounded-[18px] m-auto mt-[100px] p-6">
-        <header className="mb-4">
-            <h1 className="text-2xl font-semibold">{ "Search Record "}</h1>
-        </header>
-        
-        <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="w-full flex items-center gap-2">
-        <FormField
-          control={form.control}
-          name="query"
-          render={({ field }) => (
-            <FormItem className="flex-1">
-              <FormControl>
-                <Input  className="h-[45px]"  placeholder="Search patient" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+    <section className="sm:max-w-[512px] w-full bg-white rounded-[18px] m-auto mt-[100px] p-6">
+      <header className="mb-4">
+        <h1 className="text-2xl font-semibold">{"Search Record "}</h1>
+      </header>
 
-<Button disabled={isPending} type="submit" size={'lg'}  >Search</Button>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="w-full flex items-center gap-2"
+        >
+          <FormField
+            control={form.control}
+            name="query"
+            render={({ field }) => (
+              <FormItem className="flex-1">
+                <FormControl>
+                  <Input
+                    className="h-[45px]"
+                    placeholder="Search patient"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-      </form>
-    </Form>
+          <Button disabled={isPending} type="submit" size={"lg"}>
+            Search
+          </Button>
+        </form>
+      </Form>
 
-   {
-    record ?
-    <div className="mt-4">
-        <div className="flex gap-4 justify-between my-3">Record Id :<span className="font-semibold"> {record.recordId}</span></div>
-        <div className="flex gap-4 justify-between my-3"> Name :<span className="font-semibold"> {record.patientName}</span></div>
-        <div className="flex gap-4 justify-between my-3"> Date of Birth :<span className="font-semibold"> {record.patientDOB}</span></div>
-        <div className="flex gap-4 justify-between my-3">Gender :<span className="font-semibold"> {record.patientGender}</span></div>
-        <div className="flex gap-4 justify-between my-3">Diagnosis :<span className="font-semibold max-w-[300px] text-end"> {record.diagnosis}</span></div>
-        <div className="flex gap-4 justify-between my-3">Date Created :<span className="font-semibold"> {moment(record.createdAt).format('DD MMM, YYYY, hh:mm A')} </span></div>
-    </div> : 
-    <div>
-        
-    </div>
-   }
-   {
-    error.length > 1 && <div className="p-3 bg-red-200 rounded-sm mt-2">{error}</div>
-   }
-   
-
-    
-      </section>
-  )
-}
+      {record ? (
+        <div className="mt-4">
+          <div className="flex gap-4 justify-between my-3">
+            Record Id :<span className="font-semibold"> {record.recordId}</span>
+          </div>
+          <div className="flex gap-4 justify-between my-3">
+            {" "}
+            Name :<span className="font-semibold"> {record.patientName}</span>
+          </div>
+          <div className="flex gap-4 justify-between my-3">
+            {" "}
+            Date of Birth :
+            <span className="font-semibold"> {record.patientDOB}</span>
+          </div>
+          <div className="flex gap-4 justify-between my-3">
+            Gender :
+            <span className="font-semibold"> {record.patientGender}</span>
+          </div>
+          <div className="flex gap-4 justify-between my-3">
+            Diagnosis :
+            <span className="font-semibold max-w-[300px] text-end">
+              {" "}
+              {record.diagnosis}
+            </span>
+          </div>
+          <div className="flex gap-4 justify-between my-3">
+            Date Created :
+            <span className="font-semibold">
+              {" "}
+              {moment(record.createdAt).format("DD MMM, YYYY, hh:mm A")}{" "}
+            </span>
+          </div>
+        </div>
+      ) : (
+        <div></div>
+      )}
+      {error.length > 1 && (
+        <div className="p-3 bg-red-200 rounded-sm mt-2">{error}</div>
+      )}
+    </section>
+  );
+};
